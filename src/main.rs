54599e009e9fe3df05a68b74c41bf5cfe8024dee
Pierre-Yves Aillet <pyaillet@gmail.com>
2023-03-03 15:50:28 +0100

    working example

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,93 @@
-use std::sync::{Mutex, Arc};
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        State, WebSocketUpgrade,
+    },
+    http::Response,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router, Server,
+};
 
-use axum::{Server, Router, routing::get, extract::State};
-use sysinfo::{System, SystemExt, CpuExt};
+use sysinfo::{CpuExt, System, SystemExt};
+use tokio::sync::broadcast;
+
+type Snapshot = Vec<f32>;
 
 #[tokio::main]
 async fn main() {
+    let (tx, _) = broadcast::channel::<Snapshot>(1);
+
+    let app_state = AppState { tx: tx.clone() };
+
     let router = Router::new()
         .route("/", get(root_get))
-    .with_state(AppState { sys: Arc::new(Mutex::new(System::new())) });
+        .route("/index.mjs", get(indexmjs_get))
+        .route("/index.css", get(indexcss_get))
+        .route("/realtime/cpus", get(realtime_cpus_get))
+        .with_state(app_state.clone());
 
-    let server = Server::bind(&"0.0.0.0:7032".parse().unwrap())
-        .serve(router.into_make_service());
+    tokio::task::spawn_blocking(move || {
+        let mut sys = System::new();
+        loop {
+            sys.refresh_cpu();
+            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+            let _ = tx.send(v);
+
+            std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+        }
+    });
+
+    let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
     server.await.unwrap();
-
-    println!("Hello, world!");
 }
 
 #[derive(Clone)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    tx: broadcast::Sender<Snapshot>,
 }
 
-async fn root_get(State(state): State<AppState>) -> String {
-    use std::fmt::Write;
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
 
-    let mut s = String::new();
+    Html(markup)
+}
 
-    let mut sys = state.sys.lock().unwrap();
-    sys.refresh_cpu();
+#[axum::debug_handler]
+async fn indexmjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.mjs").await.unwrap();
 
-    for (i, cpu) in sys.cpus().iter().enumerate() {
+    Response::builder()
+        .header("content-type", "application/javascript;charset=utf-8")
+        .body(markup)
+        .unwrap()
+}
 
-        let usage = cpu.cpu_usage();
-        writeln!(&mut s, "CPU {i} {usage:.2}%").unwrap();
+#[axum::debug_handler]
+async fn indexcss_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.css").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "text/css;charset=utf-8")
+        .body(markup)
+        .unwrap()
+}
+
+#[axum::debug_handler]
+async fn realtime_cpus_get(
+    ws: WebSocketUpgrade,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|ws: WebSocket| async { realtime_cpus_stream(state, ws).await })
+}
+
+async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
+    let mut rx = app_state.tx.subscribe();
+    while let Ok(msg) = rx.recv().await {
+        let payload = serde_json::to_string(&msg).unwrap();
+        ws.send(Message::Text(payload)).await.unwrap();
     }
-
-    s
 }
